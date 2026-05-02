4877b9ea7650e0f33b2f76b1ce01220e01e73802
Voldemort-647 <tcetsaqlain@gmail.com>
2026-05-02 21:19:05 +0530

    deployment try

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["neo4j-driver"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
